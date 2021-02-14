b942bbe8e23defdcbe6111836bfa71f6c1ba0b0b
Pedro Reis <olavyo.p.lopes@gmail.com>
2021-02-14 20:55:12 -0300

    inserção da dados do usuario ao banco

--- insert.php
+++ insert.php
@@ -1,9 +1,10 @@
 <?php 
 include("conexao.php");
 
+
 function inserir($conexao, $dad){
 	
-	$script = 'INSERT INTO usuario VALUES (DEFAULT, "' . 0 . '", "' . $dad[0] . '", "' . $dad[1] . '","' . $dad[2] .'","' . $dad[3] '", "' . $dad[4] . '", "' . $dad[5] . '", "' . $dad[6] . '", "' . $dad[7] . '", "' . $dad[8] . '", "' . $dad[9] . '", "' . $dad[10] . '", "' . $dad[11] . '", "' . $dad[12] . '", "' . $dad[13] . '", ' . $dad[14] . ')';
+	$script = 'INSERT INTO usuario VALUES (DEFAULT, "' . 0 . '", "' . $dad[0] . '", "' . $dad[1] . '","' . $dad[2] .'","' . $dad[3]. '", "' . $dad[4] . '", "' . $dad[5] . '", "' . $dad[6] . '", "' . $dad[7] . '", "' . $dad[8] . '", "' . $dad[9] . '", "' . $dad[10] . '", "' . $dad[11] . '", "' . $dad[12] . '", "' . $dad[13] . '", ' . $dad[14] . ')';
 
 	$insere = $conexao->query($script);
 	if(!$insere){
@@ -17,11 +18,12 @@ function inserir($conexao, $dad){
 	}
 }
 
+$dad = array($_GET['nome'], $_GET['sobrenome'],$_GET['data_nascimento'],$_GET['sexo'],$_GET['cpf'],$_GET['cep'],$_GET['telefone'],$_GET['endereco'],$_GET['num_endereco'],$_GET['complemento'],$_GET['cidade'],$_GET['bairro'],$_GET['estado'],$_GET['email'],$_GET['senha']);
 
-$dad = array($_GET['nome'], $_GET['sobrenome'],$_GET['data_nascimento'],$_GET['sexo'],$_GET['cpf'],$_GET['cep'],$_GET['telefone'],$_GET['endereco'],$_GET['nun_endereco'],$_GET['complemento'],$_GET['cidade'],$_GET['bairro'],$_GET['estado'],$_GET['email'],$_GET['senha']);
+
 
 inserir($conexao,$dad);
 
-//header('Location: /ProjectWeb/login.php');
+header('Location: /ProjectInt/login.php');
 
  ?>
