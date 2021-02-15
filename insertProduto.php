40a82efbcde8aeb297f0ce6f4f8ff20496c348c8
Pedro Reis <olavyo.p.lopes@gmail.com>
2021-02-15 20:33:17 -0300

    codigo de inserção de dados ao banco

--- insertProduto.php
+++ insertProduto.php
@@ -3,9 +3,9 @@ include("conexao.php");
 
 function inserir($conexao, $dad, $destino){
 	
-	$script = 'INSERT INTO produto VALUES (DEFAULT, "' . $dad[0] . '", "' . $dad[1] . '", "' . $dad[2] . '","' . $dad[3] .'","' . $dad[4] .'","' . $dad[5] .'","' . $dad[6] .'","' . $destino . '",' . $dad[7] .')';
+	$script = 'INSERT INTO produto VALUES (DEFAULT, "' . $dad[0] . '", "' . $dad[1] . '", "' . $dad[2] . '","' . $dad[3] .'","' . $dad[4] .'","' . $dad[5] .'","' . $dad[6] .'","' . $destino . '","' . $dad[7] .'")';
 
-	$insere = $conexao->query($script);
+	/*$insere = $conexao->query($script);
 	if(!$insere){
 		echo "<br>Inserção incorreta! Deu erro!<br>";
 		echo $conexao->error;
@@ -19,10 +19,21 @@ function inserir($conexao, $dad, $destino){
 		echo '<script>alert("Cadastro feito com sucesso!")</script>';
 		echo '<script>window.location="addProduto.php"</script>';
 	}
+}*/
+$insere = $conexao->query($script);
+if(!$insere){
+	echo "<br>Inserção incorreta! Deu erro!<br>";
+	echo $conexao->error;
+	echo "<br><br>";
+	echo $script;
+	echo "<br>";
+}else{
+	echo "<br>Inserção Realizada corretamente!";
+}
 }
 
 
-$dad = array($_GET['nomeProduto'], $_GET['lancamentoProduto'], $_GET['DescricaoProdutoMenu'], $_GET['precoProduto'], $_GET['estoqueProduto'], $_GET['numTamanhos'], $_GET['tamanhosProduto'], $_GET['descricaoProduto']);
+$dad = array($_GET['nomeProduto'], $_GET['lancamentoProduto'], $_GET['descricaoProdutoMenu'], $_GET['precoProduto'], $_GET['estoqueProduto'], $_GET['numTamanhos'], $_GET['tamanhosProduto'], $_GET['descricaoProduto']);
 $dir= "Img/";
 $file= $_FILES['foto'];
 $destino= "$dir".$file["name"];
@@ -31,4 +42,4 @@ move_uploaded_file($file['tmp_name'], $destino);
 
 inserir($conexao,$dad,$destino);
 
- ?>
+?>
