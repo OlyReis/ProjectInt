d469e2b1799ee74df461e69a8c58063d998779eb
christhian12rv <christhian11rv@hotmail.com>
2021-02-15 20:03:33 -0300

    Troca do array por variáveis

--- insert.php
+++ insert.php
@@ -2,11 +2,12 @@
 include("conexao.php");
 
 
-function inserir($conexao, $dad){
+function inserir($conexao, $nome, $sobrenome, $data_nascimento, $sexo, $cpf, $cep, $telefone, $endereco, $num_endereco, $complemento, $cidade, $bairro, $estado, $email, $senha) {
 	
-	$script = 'INSERT INTO usuario VALUES (DEFAULT, "' . 0 . '", "' . $dad[0] . '", "' . $dad[1] . '","' . $dad[2] .'","' . $dad[3]. '", "' . $dad[4] . '", "' . $dad[5] . '", "' . $dad[6] . '", "' . $dad[7] . '", "' . $dad[8] . '", "' . $dad[9] . '", "' . $dad[10] . '", "' . $dad[11] . '", "' . $dad[12] . '", "' . $dad[13] . '", ' . $dad[14] . ')';
+	$script = 'INSERT INTO usuario VALUES (DEFAULT, "' . 0 . '", "' . $nome . '", "' . $sobrenome . '","' . $data_nascimento .'","' . $sexo . '", "' . $cpf . '", "' . $cep . '", "' . $telefone . '", "' . $endereco . '", "' . $num_endereco . '", "' . $complemento . '", "' . $cidade . '", "' . $bairro . '", "' . $estado . '", "' . $email . '", ' . $senha . ')';
 
 	$insere = $conexao->query($script);
+
 	if(!$insere){
 		echo "<br>Inserção incorreta! Deu erro!<br>";
 		echo $conexao->error;
@@ -18,11 +19,25 @@ function inserir($conexao, $dad){
 	}
 }
 
-$dad = array($_POST['nome'], $_POST['sobrenome'],$_POST['data_nascimento'],$_POST['sexo'],$_POST['cpf'],$_POST['cep'],$_POST['telefone'],$_POST['endereco'],$_POST['num_endereco'],$_POST['complemento'],$_POST['cidade'],$_POST['bairro'],$_POST['estado'],$_POST['email'],$_POST['senha']);
+$nome = $_POST['nome'];
+$sobrenome = $_POST['sobrenome'];
+$data_nascimento = $_POST['data_nascimento'];
+$sexo = $_POST['sexo'];
+$cpf = $_POST['cpf'];
+$cep = $_POST['cep'];
+$telefone = $_POST['telefone'];
+$endereco = $_POST['endereco'];
+$num_endereco = $_POST['num_endereco'];
+$complemento = $_POST['complemento'];
+$cidade = $_POST['cidade'];
+$bairro = $_POST['bairro'];
+$estado = $_POST['estado'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
 
 
-inserir($conexao,$dad);
+inserir($conexao, $nome, $sobrenome, $data_nascimento, $sexo, $cpf, $cep, $telefone, $endereco, $num_endereco, $complemento, $cidade, $bairro, $estado, $email, $senha);
 
 header('Location: /ProjectInt/login.php');
 
