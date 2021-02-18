40ac21d7336423bfaa82c4ad6b36f72bdc1852b8
christhian12rv <christhian11rv@hotmail.com>
2021-02-18 16:01:58 -0300

    Ajuste da margem do Login

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
       <?php
       if (isset($_SESSION['email'])) {
         echo '
-        <div class="dropdown show" style="margin-right: 40px;">
+        <div class="dropdown show" style="margin-right: 65px;">
           <a class="text-light dropdown-toggle font-weight-bold" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             ' . $nome . '
           </a>
