42f56107542430b64a11f3bc7d004793c5a37106
christhian12rv <christhian11rv@hotmail.com>
2021-02-18 17:44:12 -0300

    PHP para Deletar Conta

---
+++ acaoDeletarConta.php
@@ -0,0 +1,35 @@
+<?php
+  include('conexao.php');
+
+  $senhaInput = $_POST['senha'];
+
+  session_start();
+  if (isset($_SESSION['email'])) 
+  { 
+    $logado = $_SESSION['email'];
+    $script = 'SELECT * FROM usuarios WHERE email = "' . $logado . '"';
+    $consulta = $conexao->query($script);
+    $linha = $consulta->fetch_array(MYSQLI_ASSOC);
+    $id = $linha['id'];
+    $senha = $linha['senha'];
+
+    if ($senhaInput == $senha) {
+
+      $script = 'DELETE FROM usuarios WHERE id = "' . $id . '"';
+      $deleta = $conexao->query($script);
+      if ($deleta) {
+        session_destroy();
+        unset($_SESSION['email']);
+        unset($_SESSION['senha']);
+        echo "<script> alert('Conta deletada com sucesso!')</script>";
+        echo '<script>window.location="index.php"</script>';
+      } else {
+        echo "<script> alert('Erro ao deletar conta')</script>";
+        echo '<script>window.location="deletarConta.php"</script>';
+      }
+    } else {
+      echo "<script> alert('Senha incorreta.')</script>";
+      echo '<script>window.location="deletarConta.php"</script>';
+    }
+  }
+?>
