d66e4f6f62ff23ef4e19eeca3e5a612d3e494a3d
christhian12rv <christhian11rv@hotmail.com>
2021-02-18 17:43:28 -0300

    HTML para deletar Conta

--- deletarConta.php
+++ deletarConta.php
@@ -1,6 +1,17 @@
-<?php 
-include("conexao.php");
- ?>
+<?php
+  include('conexao.php');
+
+  session_start();
+  if (isset($_SESSION['email'])) 
+  { 
+    $logado = $_SESSION['email'];
+    $script = 'SELECT * FROM usuarios WHERE email = "' . $logado . '"';
+    $consulta = $conexao->query($script);
+    $linha = $consulta->fetch_array(MYSQLI_ASSOC);
+    $nome = $linha['nome'];
+  }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,7 +32,7 @@ include("conexao.php");
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-right: 57px;">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
           <a class="nav-link " href="index.php">Menu<span class="sr-only"></span></a>
@@ -47,3 +58,62 @@ include("conexao.php");
         </div>
       </div>
     </nav>
+
+    <h1 class="col-12">Deletar Conta</h1><br>
+    <div class="container col-12 divcadastro rounded border border-primary mb-5">
+      <form class="needs-validation m-4"method="POST" action="acaoDeletarConta.php">
+        <div class="row"><p> Olá, &nbsp;</p><p class="font-weight-bold"><?php echo $nome; ?></p></div>
+        <p>É uma pena que você queira excluir sua conta.</p>
+        <p>Lembre-se que o processo não pode ser revertido.</p>
+        <p>Diga-nos porquê quer exclui-la.</p>
+        <textarea rows="5" cols="100" style="resize: none;" maxlength="316"></textarea>
+        <hr>
+        <p class="mt-4">Para continuar, digite a sua senha novamente.</p>
+        <input type="password" name="senha" class="mb-3 inputpw"><br>
+        <button class="text-light btn btn-danger font-weight-bold"><svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" style="margin-left: -6px; margin-top: -2px;">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>Deletar Conta</button>
+      </form>
+    </div>
+
+    <footer class=" py-5">
+      <div class="row">
+        <div class="col- col-12 col-md">
+          <small class="d-block mb-3 text-muted">&copy; 2020</small>
+        </div>
+        <div class="col- col-6 col-md">
+          <h5>Sobre Nós</h5>
+          <ul class="list-unstyled text-small">
+            <li><a href="sobre.php#SD">Informações da Empresa</a></li>
+            <li><a href="sobre.php">Informações dos Desenvolvedores do Site</a></li>
+          </ul>
+        </div>
+        <div class="col- col-6 col-md">
+          <h5>Contato</h5>
+          <ul class="list-unstyled text-small">
+            <li><a href="contato.php">E-mail</a></li>
+            <li><a href="contato.php#SD">WhatsApp</a></li>
+          </ul>
+        </div>
+        <div class="col- col-6 col-md">
+          <h5>Redes Sociais</h5>
+          <ul class="list-unstyled text-small">
+            <li><a target="_blank" href="https://www.facebook.com">Facebook</a></li>
+            <li><a target="_blank" href="https://www.instagram.com/modaz">Instagram</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+
+    <span class="darkmode-ignore"><span>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.5/lib/darkmode-js.min.js"></script>
+    <script>
+      new Darkmode().showWidget();
+    </script>
+    <script type="JS/Bootstrap.mim.js"></script>
+    <script type="text/javascript" src="JS/jquery.mask.js"></script>
+    <script type="text/javascript" src="JS/jquery.mask.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </html>
