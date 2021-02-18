618bcef094d3ecf2cb68a772b02b582497c56d30
christhian12rv <christhian11rv@hotmail.com>
2021-02-18 12:10:42 -0300

    Alert e Redirecionamento

--- acaoVerificaLogin.php
+++ acaoVerificaLogin.php
@@ -11,9 +11,13 @@
     echo 'Existe';
     $_SESSION['email'] = $email;
     $_SESSION['senha'] = $senha;
+    echo "<script> alert('Login Conectado!')</script>";
+    echo '<script>window.location="index.php"</script>';
   } else {
     echo 'Não Existe';
     unset ($_SESSION['email']);
     unset ($_SESSION['senha']);
+    echo "<script> alert('Email ou senha incorretos.') </script>";
+    echo '<script>window.location="login.php"</script>';
   }
 ?>
