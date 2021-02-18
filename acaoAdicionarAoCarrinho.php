44b289c37c9f50568339d920e00295b1f4ec9dab
christhian12rv <christhian11rv@hotmail.com>
2021-02-18 18:28:04 -0300

    PHP para linkar com o Login caso o usuário não esteja logado

---
+++ acaoAdicionarAoCarrinho.php
@@ -0,0 +1,14 @@
+<?php
+  include('conexao.php');
+
+  session_start();
+  if (isset($_SESSION['email'])) 
+  { 
+    $logado = $_SESSION['email'];
+    $script = 'SELECT * FROM usuarios WHERE email = "' . $logado . '"';
+    $consulta = $conexao->query($script);
+    $linha = $consulta->fetch_array(MYSQLI_ASSOC);
+  } else {
+    echo '<script> window.location = "login.php"; </script>';
+  }
+?>
