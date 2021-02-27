7d60a3577cc9e7e425da2d4c152053e0db9d73fb
christhian12rv <christhian11rv@hotmail.com>
2021-02-27 18:39:03 -0300

    PHP feito

--- acaoAdicionarAoCarrinho.php
+++ acaoAdicionarAoCarrinho.php
@@ -5,16 +5,22 @@
   if (isset($_SESSION['email'])) 
   { 
     $id = $_GET['id'];
-    
+    $estoque = $_GET['estoque'];
+
     if (strpos($_SESSION['idsCarrinho'], $id) !== false) {
       echo "Já existe no carrinho";
       echo '<script> alert("Produto já adicionado ao carrinho."); </script>';
       echo '<script> window.location = "paginaProduto.php?id=' . $id . '"; </script>';
     } else {
-      $_SESSION['nItensCarrinho'] += 1;
-      $_SESSION['idsCarrinho'] = $_SESSION['idsCarrinho'] . $id . ",";
-      echo $_SESSION['idsCarrinho'];
-      echo '<script> window.location = "paginaProduto.php?id=' . $id . '"; </script>';
+      if ($estoque == 0) {
+        echo '<script> alert("Produto com 0 de estoque."); </script>';
+        echo '<script> window.location = "paginaProduto.php?id=' . $id . '"; </script>';
+      } else {
+        $_SESSION['nItensCarrinho'] += 1;
+        $_SESSION['idsCarrinho'] = $_SESSION['idsCarrinho'] . $id . ",";
+        echo $_SESSION['idsCarrinho'];
+        echo '<script> window.location = "paginaProduto.php?id=' . $id . '"; </script>';
+      }
     }
   } else {
     echo '<script> window.location = "login.php"; </script>';
