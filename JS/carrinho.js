c3a94243f889e128812a01bcee1ca8076d60ce72
christhian12rv <christhian11rv@hotmail.com>
2021-02-27 18:38:38 -0300

    Integração com PHP e MySQL

--- JS/carrinho.js
+++ JS/carrinho.js
@@ -1,54 +1,29 @@
-$("#menosQtd").click( function() {
+function menosQtd($preco, $idProduto) {
+  if (document.getElementById("inputQTD" + $idProduto).value > 1) {
+    document.getElementById("inputQTD" + $idProduto).value = parseInt(document.getElementById("inputQTD" + $idProduto).value) -1;
 
-  var valorQtd = document.getElementById("qtd").value;
-
-  if (valorQtd > 1) {
-    document.getElementById("qtd").value = parseInt(valorQtd) - 1;
+    $subTotal = parseFloat(document.getElementById("subtotal").innerHTML.replace(",", ".").substring(3,document.getElementById("subtotal").innerHTML.length));
+    $subTotal = $subTotal - $preco;
+    $subTotal = $subTotal.toFixed(2);
+    document.getElementById("subtotal").innerHTML = "R$ " + $subTotal.toString().replace(".",",");
+    document.getElementById("valorTotal").innerHTML = "R$ " + $subTotal.toString().replace(".",",");
   }
 
-  valorQtd = document.getElementById("qtd").value;
-
-  var valorSubTotal = parseFloat(document.getElementById("valorProduto").innerHTML.substr(3,document.getElementById("valorProduto").lenght).replace(",", "."));
-  for (i = 1; i < valorQtd; i++) {
-    valorSubTotal += parseFloat(document.getElementById("valorProduto").innerHTML.substr(3,document.getElementById("valorProduto").lenght).replace(",", "."));
-  }
-  document.getElementById("subtotal").innerHTML = "R$ " + valorSubTotal.toFixed(2).replace(".",",");
-  document.getElementById("valorTotal").innerHTML = "R$ " + valorSubTotal.toFixed(2).replace(".",",");
-})
-
-$("#maisQtd").click( function() {
-  var valorQtd = document.getElementById("qtd").value;
-  var valorEstoque = parseInt(document.getElementById("numEstoque").innerHTML.substr(9,1));
-
-  if (valorQtd < valorEstoque) {
-    document.getElementById("qtd").value = parseInt(document.getElementById("qtd").value) + 1;
-  }
-
-  valorQtd = document.getElementById("qtd").value;
-
-  var valorSubTotal = parseFloat(document.getElementById("valorProduto").innerHTML.substr(3,document.getElementById("valorProduto").lenght).replace(",", "."));
-  for (i = 1; i < valorQtd; i++) {
-    valorSubTotal += parseFloat(document.getElementById("valorProduto").innerHTML.substr(3,document.getElementById("valorProduto").lenght).replace(",", "."));
-  }
-  document.getElementById("subtotal").innerHTML = "R$ " + valorSubTotal.toFixed(2).replace(".",",");
-  document.getElementById("valorTotal").innerHTML = "R$ " + valorSubTotal.toFixed(2).replace(".",",");
-})
-
-function deletaProduto(divProduto) {
-  divProduto.parentNode.parentNode.parentNode.remove();
-  verificaCarrinho();
 }
 
-function verificaCarrinho() {
-  document.getElementById("numItensSubtotal").innerHTML = "Subtotal (0 itens)";
-  document.getElementById("subtotal").innerHTML = "R$ 00,00";
-  document.getElementById("valorTotal").innerHTML = "R$ 00,00";
+function maisQtd($preco, $idProduto, $estoqueProduto) {
+    if (parseInt(document.getElementById("inputQTD" + $idProduto).value) < $estoqueProduto) {
+      document.getElementById("inputQTD" + $idProduto).value = parseInt(document.getElementById("inputQTD" + $idProduto).value) + 1;
+
+      $subTotal = parseFloat(document.getElementById("subtotal").innerHTML.replace(",", ".").substring(3,document.getElementById("subtotal").innerHTML.length));
+      $subTotal = $subTotal + $preco;
+      $subTotal = $subTotal.toFixed(2);
+      document.getElementById("subtotal").innerHTML = "R$ " + $subTotal.toString().replace(".",",");
+      document.getElementById("valorTotal").innerHTML = "R$ " + $subTotal.toString().replace(".",",");
+    }
+  
 }
 
-$("#continuarBtn").click(function() {
-  if (document.getElementById("tamanhosProduto").value != "") {
-    window.location.href = "compra.html";
-  } else {
-    document.getElementById("validarPag").innerHTML = "<p class='text-light text-center mx-auto'> Por favor, escolha os Tamanhos <p>";
-  }
-})
+function deletaProduto(idProduto) {
+  window.location = 'deletaProdutoCarrinho.php?id=' + idProduto;
+}
