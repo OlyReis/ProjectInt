d83100ee614c8ebaa8b5c80d2289a6f51d58041b
christhian12rv <christhian11rv@hotmail.com>
2021-02-27 15:44:03 -0300

    Alteração do php do textarea

--- alterarProduto.php
+++ alterarProduto.php
@@ -35,9 +35,9 @@ if (!$consulta) {
   $descricaoMenu = $linha['descricaoM'];
   $precoProduto = $linha['preco']; 
   $quantidadeProduto = $linha['qtd'];
-  $numeroTamProduto = $linha['NTP'];
+  $numeroTamProduto = $linha['ntp'];
   $tamanho = $linha['tamanho'];
-  $discricaoProduto = $linha['discricaoP'];
+  $descricaoProduto = $linha['descricaoP'];
 }
 
 ?>
@@ -172,7 +172,7 @@ if (!$consulta) {
       <div class="form-row mb-3 mt-3">
         <div class="col">
           <label for="labelDescricaoProduto">Descrição do Produto</label>               
-          <textarea class="form-control textArea" id="textArea" rows="15" name="descricaoProduto" placeholder="Digite a Descrição do Produto" required></textarea>
+          <textarea class="form-control textArea" id="textArea" rows="15" name="descricaoProduto" placeholder="Digite a Descrição do Produto" required> <?php echo $descricaoProduto; ?> </textarea>
           <div class="invalid-feedback" style="border-top: 2px solid red">
             Por favor insira uma Descrição válida.
           </div>
@@ -226,5 +226,4 @@ if (!$consulta) {
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script type="text/javascript" src="JS/jquery.mask.min.js"></script>
-
   </html>
