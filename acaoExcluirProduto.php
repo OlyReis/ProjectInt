e48813aa467e7f89e7765a706612ac144cc95dd1
christhian12rv <christhian11rv@hotmail.com>
2021-02-28 16:44:09 -0300

    Auto Increment arrumado

--- acaoExcluirProduto.php
+++ acaoExcluirProduto.php
@@ -2,6 +2,7 @@
   include("conexao.php");
   echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css"> <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script> <link rel="stylesheet" type="text/css" href="CSS/bootstrap.min.css">';
 
+  session_start();
   $id = $_GET['id'];
   $nomeProduto = $_GET['nome'];
   $script = 'DELETE FROM produtos WHERE id = "' . $id . '"';
@@ -11,6 +12,9 @@
     $consulta = $conexao->query($script);
     $script = "UPDATE `produtos` SET `produtos`.`id` = @count:= @count + 1";
     $consulta = $conexao->query($script);
+ 
+    $_SESSION['idsCarrinho'] = '';
+    $_SESSION['nItensCarrinho'] = 0;    
 
     echo "<script> $.confirm({type: 'red', title: 'Excluir Produto', content: 'Produto " . $nomeProduto . " excluido com sucesso!', buttons: { Ok: { btnClass: 'btn-red', action: function () {"; echo 'window.location="indexAlterarProduto.php"'; echo " }}}});</script>";
   } else {
