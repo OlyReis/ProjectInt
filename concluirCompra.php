1cba02f203e0e8fa26fbcc6ccd2848842e2f30fb
christhian12rv <christhian11rv@hotmail.com>
2021-02-28 23:10:43 -0300

    Pegada as variáveis do POST

--- concluirCompra.php
+++ concluirCompra.php
@@ -1,3 +1,51 @@
+<?php
+  include('conexao.php');
+
+  session_start();
+  if (isset($_SESSION['email'])) 
+  { 
+    $logado = $_SESSION['email'];
+    $script = 'SELECT * FROM usuarios WHERE email = "' . $logado . '"';
+    $consulta = $conexao->query($script);
+    $linha = $consulta->fetch_array(MYSQLI_ASSOC);
+    $idUsuario = $linha['id'];
+    $nome = $linha['nome'];
+    $cidade = $linha['cidade'];
+    $estado = $linha['estado'];
+    $cep = $linha['cep'];
+    $telefone = $linha['telefone'];
+
+    $OpcaoEnvio = $_POST['opcaoEnvio'];
+
+    $idTodosProdutos = $_POST['idTodosProdutos'];
+    $idCadaProduto = explode(",",$idTodosProdutos);
+    echo $idCadaProduto[0];
+    $qtdTodosProdutos = $_POST['qtdTodosProdutos'];
+    $qtdCadaProduto = explode(",",$qtdTodosProdutos);
+    echo $qtdCadaProduto[0];
+    $tamanhoTodosProdutos = $_POST['tamanhoTodosProdutos'];
+    $tamanhoCadaProduto = explode(",",$tamanhoTodosProdutos);
+    echo $tamanhoCadaProduto[0];
+    $valorTotal = $_POST['valorTotal'];
+    $modoPagamento = $_POST['opcaoPagamento'];
+    date_default_timezone_set('America/Sao_Paulo');
+    $dataCompra = date("Y-m-d");
+
+    if ($_SESSION['nItensCarrinho'] != 0) {
+      $nItensCarrinho = $_SESSION['nItensCarrinho'];
+      $idsCarrinho = $_SESSION['idsCarrinho'];
+      $idCadaProdutoCarrinho = explode(",", $idsCarrinho);
+    } else {
+      $_SESSION['nItensCarrinho'] = 0;
+      $nItensCarrinho = $_SESSION['nItensCarrinho'];
+    }
+
+  } else {
+    $_SESSION['nItensCarrinho'] = 0;
+    $nItensCarrinho = $_SESSION['nItensCarrinho'];
+  } 
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -32,7 +80,7 @@
       </ul>
       <a href="carrinho.php" class="mr-3"><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cart2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
-      </svg><span class="numItensCarrinho" name="numItensCarrinho">0</span><a>
+      </svg><span class="numItensCarrinho" name="numItensCarrinho"><?php echo $nItensCarrinho; ?></span><a>
         <button type="button" class="btn btn-dark"><a href="login.php">Login<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-square ml-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
           <path fill-rule="evenodd" d="M2 15v-1c0-1 1-4 6-4s6 3 6 4v1H2zm6-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
